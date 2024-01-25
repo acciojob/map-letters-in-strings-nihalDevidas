726bcf832c891df65f43b35468de561df6cf8c27
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-01-25 21:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+const mpLetters = (str)=>{
+
+	let mp = new Map()
+
+	for(let i=0; i<str.length; i++){
+		let ch = str.charAt(i)
+		if(mp.has(ch)){
+			let arr = mp.get(ch);
+			 arr.push(i)
+			mp.set(ch,[...arr])
+		}
+		else{
+			mp.set(ch,[i])
+		}
+	}
+	return mp;
+}
+
